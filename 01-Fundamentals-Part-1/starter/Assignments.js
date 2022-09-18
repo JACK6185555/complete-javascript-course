da6c6c1e6dae6a15747d97509bd63f9690f268a9
JACK61855555 <wongkalok0618@gmail.com>
2022-09-18 23:15:39 +0800

    Update Assignments.js

--- 01-Fundamentals-Part-1/starter/Assignments.js
+++ 01-Fundamentals-Part-1/starter/Assignments.js
@@ -9,3 +9,16 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
+//Parctice 2
+
+alert('Data type')
+
+let isIsland=true
+let iscountry='Hong Kong'
+let language="Chinese"
+let ispopulation=7000000
+
+console.log(typeof isIsland)
+console.log(typeof iscountry)
+console.log(typeof language)
+console.log(typeof ispopulation)
