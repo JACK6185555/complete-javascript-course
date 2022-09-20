277c6455fc2f1fb206fd6ced3db54386a785797b
JACK61855555 <wongkalok0618@gmail.com>
2022-09-20 10:03:33 +0800

    Update Assignments.js

--- 01-Fundamentals-Part-1/starter/Assignments.js
+++ 01-Fundamentals-Part-1/starter/Assignments.js
@@ -63,18 +63,7 @@ console.log(ispopulation>Finland_population)
 some_population=3300000
 console.log(population<some_population)
 
-alert('Challenge #1')
-massMark=78;
-heightMark=1.69;
-massJohn=92;
-heightJohn=1.95;
-Mark_BMI=massMark/heightMark**2
-John_BMI=massJohn/(heightJohn*heightJohn);
-console.log(Mark_BMI,John_BMI);
 
-markHigherBMI=Mark_BMI>John_BMI;
-console.log(markHigherBMI);
-let description='Portugal is in Europe, and its 11 million people speak portuguese'
 
 //Strings and Template Literals
 alert('Strings and Template Literals')
@@ -88,16 +77,7 @@ if(ispopulation>3300000){
 }else{
     console.log(`'Portugal's population is 22 million below average`)
 }
-alert('Challenge #2');
-if(Mark_BMI>John_BMI){
-    console.log(`Mark's BMI is higher than John's!`);
-}else{
-    console.log(`John's BMI is higher than Mark's!`);
-}
 
-console.log(`Mark's BMI (${Mark_BMI}) is higher than John's (${John_BMI})!`);
-
-alert('Type Conversion and Coercion')
 //Type conversion
 const inputYear='1991';
 console.log(Number(inputYear),inputYear);
@@ -170,16 +150,3 @@ if (city_language==='english'&&city_people<50&& !land){
     console.log(`You shouldn't live in ${country}`)
 }
 
-//Challenge #3
-Dolphins_score=(97+112+101)/3;
-Koalas_score=(109+95+106)/3;
-
-if (Dolphins_score>Koalas_score&&Dolphins_score>=100){
-    console.log('Dolphins win the trophy🏆')
-}else if(Koalas_score>Dolphins_score&&Koalas_score>=100){
-    console.log('Koalas win the trophy🏆')
-}else if(Dolphins_score===Koalas_score&&Dolphins_score>=100&&Koalas_score>=100){
-    console.log('Both  win the trophy🙌')
-}else{
-    console.log('No one win 😥')
-}
