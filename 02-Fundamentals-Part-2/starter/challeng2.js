2cc94c18f7d7cbfb7e1d6247f4da773c9911c1b9
JACK61855555 <wongkalok0618@gmail.com>
2022-09-23 02:18:34 +0800

    Update challeng2.js

--- 02-Fundamentals-Part-2/starter/challeng2.js
+++ 02-Fundamentals-Part-2/starter/challeng2.js
@@ -15,3 +15,39 @@ const checkWinner = function (avgDolhins, avgKoalas) {
   }
 };
 checkWinner(avgDolhins, avgKoalas);
+alert("Challenge#2"); //ask momo wong
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.2 : bill * 0.15;
+}
+
+bill = [125, 555, 44];
+tip = [calcTip(bill[0]), calcTip(bill[1], calcTip(bill[2]))];
+total = bill + tip;
+console.log(bill, tip, total);
+alert("Challeng#3"); //ask momo wong
+const mark = {
+  full_name: "Mark Miller",
+  mass: 78,
+  heigth: 1.69,
+  mark_bmi: function () {
+    this.bmi = this.mass / this.heigth ** 2;
+  },
+};
+const john = {
+  full_name: "John Smith",
+  mass: 92,
+  heigth: 1.95,
+  john_bmi: function () {
+    this.bmi = this.mass / this.heigth ** 2;
+  },
+};
+mark.mark_bmi();
+john.john_bmi();
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(`Mark's BMI(${mark.bmi})is higher than John's(${john.bmi})`);
+} else {
+  console.log(`John's BMI(${john.bmi})is higher than Mark's(${mark.bmi})`);
+}
+alert("Challeng#4");
