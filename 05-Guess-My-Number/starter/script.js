c858b21fc3e4633cedc49ffb4285235073171b1b
JACK61855555 <wongkalok0618@gmail.com>
2022-10-04 22:34:16 +0800

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,41 @@
 'use strict';
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number ✨';
 document.querySelector('.number').textContent = '13';
 document.querySelector('.score').textContent = '10';
-
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+*/
+const number = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = number;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number ❌';
+  } else if (guess === number) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Correct Number ✨';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!😥';
+    }
+  } else if (guess > number) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈Too high !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!😥';
+    }
+  } else if (guess < number) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📉Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = 'You lost the game!😥';
+  }
+});
