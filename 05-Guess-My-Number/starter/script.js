d7ce9d9b90e9c07081d8121fdec96e567262a347
JACK61855555 <wongkalok0618@gmail.com>
2022-10-05 23:47:22 +0800

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,7 +8,6 @@ document.querySelector('.guess').value = 23;
 */
 const number = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = number;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -17,6 +16,8 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === number) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Correct Number ✨';
+      document.querySelector('.number').textContent = number;
+
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
     } else {
