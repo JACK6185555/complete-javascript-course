7c82628a76a9267d0dcf6d4299bdd7e02f2ec514
JACK61855555 <wongkalok0618@gmail.com>
2022-10-13 23:03:40 +0800

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -6,52 +6,50 @@ document.querySelector('.number').textContent = '13';
 document.querySelector('.score').textContent = '10';
 document.querySelector('.guess').value = 23;
 */
-let number = Math.trunc(Math.random() * 20 + 1);
+let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
-const displayMessage = function (message) {
+console.log(number);
+//
+const printmessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+const displayscore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+//
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
-    displayMessage('No number ❌');
+    printmessage('⛔️ No number!');
   } else if (guess === number) {
-    if (score > 0) {
-      displayMessage('Correct Number ✨');
-      document.querySelector('.number').textContent = number;
-
-      document.querySelector('body').style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem';
-
-      if (score > highscore) {
-        highscore = score;
-        document.querySelector('.highscore').textContent = highscore;
-      }
+    printmessage('Correct Number ✨');
+    document.querySelector('body').style.background = '#60b347';
+    document.querySelector('.number').textContent = number;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
   } else if (guess !== number) {
     if (score > 1) {
-      displayMessage(guess > number ? '📈 Too high!' : '📉 Too low!');
+      printmessage(guess > number ? '📈 Too high!' : '📉 Too low!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayscore(score);
     } else {
-      displayMessage('💥 You lost the game!');
-      document.querySelector('.score').textContent = 0;
+      printmessage('Game over 😥');
+      displayscore(0);
     }
   }
 });
-
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  number = Math.trunc(Math.random() * 20 + 1);
-
-  //
-  displayMessage('Start guessing...');
-  document.querySelector('.score').textContent = score;
+  Math.trunc(Math.random() * 20) + 1;
+  printmessage('start guessing...');
+  displayscore(score);
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
 
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.background = '#222';
 });
