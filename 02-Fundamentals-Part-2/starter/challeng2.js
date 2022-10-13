33a6a02c489375f95dcca538917bb6437f183ada
JACK61855555 <wongkalok0618@gmail.com>
2022-10-13 19:34:35 +0800

    Update challeng2.js

--- 02-Fundamentals-Part-2/starter/challeng2.js
+++ 02-Fundamentals-Part-2/starter/challeng2.js
@@ -63,3 +63,12 @@ for (let i = 0; i < Bills.length; i++) {
     `The bill was ${Bills[i]}, the tips was ${tips}, and the total was ${total}`
   );
 }
+
+const calcAverage_2 = function (arr) {
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage_2([3, 4, 5]));
