4527424717d5249f5b3145376014ccc72451fa81
JACK61855555 <wongkalok0618@gmail.com>
2022-10-13 19:25:07 +0800

    Update challenge.js

--- 03-Developer-Skills/starter/challenge.js
+++ 03-Developer-Skills/starter/challenge.js
@@ -1,15 +1,14 @@
-date_1 = [17, 21, 23];
-date_2 = [12, 5, 15, 0, 4];
-console.log(`....${date_1[0]}ºC....${date_1[1]}ºC....${date_1[2]}ºC`);
-const printForecast = function (arr) {
-  for (i = 0; i < arr.length; i++) {
-    console.log(`...${arr[i]}ºC in ${i + 1} days`);
+date1 = [17, 21, 23];
+date2 = [12, 5, -5, 0, 4];
+
+const printForcast = function (a) {
+  let text = "";
+  for (let i = 0; i < a.length; i++) {
+    text = `...${a[i]}ºC in ${i + 1} days`;
+    console.log(text);
   }
 };
-printForecast(date_1);
-
-for (i = 0; i < date_2.length; i++) {
-  console.log(`...${date_2[i]}`);
-}
-
-printForecast(date_2);
+k = printForcast(date1);
+q = printForcast(date2);
+console.log(k);
+console.log(q);
