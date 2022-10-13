4347a318db79c4bdd056d40d5256b832b499471e
JACK61855555 <wongkalok0618@gmail.com>
2022-10-13 19:04:28 +0800

    Update challenge1.js

--- 01-Fundamentals-Part-1/starter/challenge1.js
+++ 01-Fundamentals-Part-1/starter/challenge1.js
@@ -1,47 +1,41 @@
-//challenge 1
-alert("challenge #1");
-Mark_mass = Number(prompt("What's Mark mass?"));
-Mark_height = Number(prompt("What's Mark's height?"));
+//challenge #1
+Mark_mass = Number(prompt("Mark'mass: "));
+Mark_height = Number(prompt("Mark's height: "));
 
-John_mass = Number(prompt("What's John mass?"));
-John_height = Number(prompt("What's John height?"));
+John_mass = Number(prompt("John's mass: "));
+John_height = Number(prompt("John's height: "));
 
 Mark_BMI = Mark_mass / Mark_height ** 2;
-John_BMI = John_mass / (John_height * John_height);
+John_BMI = John_mass / (Mark_height * Mark_height);
 
-Number(Mark_BMI) > Number(John_BMI) ? console.log(true) : console.log(false);
+markHigherBMI = Mark_BMI > John_BMI;
+console.log(markHigherBMI);
 
-//challenge 2
-alert("challenge #2");
-if (Mark_BMI > John_BMI) {
-  console.log(`Mark's BMI(${Mark_BMI}) is higher than John's(${John_BMI})! `);
+//challenge #2
+if (markHigherBMI === true) {
+  console.log("Mark's BMI is higher than John's!");
 } else {
-  console.log(`John's BMI(${John_BMI}) is higher than Mark's(${Mark_BMI})! `);
+  console.log("John's BMI is higher than Mark's!");
 }
 
-//challenge 3
-alert(`challenge #3`);
-const calcaverage = function (a, b, c) {
-  return (a + b + c) / 3;
-};
-Dolphins_score = calcaverage(96, 108, 89);
-Koalas_score = calcaverage(88, 91, 110);
+//challenge #3
+Dolphins_score = Number(prompt("Dolphins's score: "));
+Koalas_score = Number(prompt("Koalas's score: "));
 
-if (Dolphins_score > Koalas_score && Dolphins_score >= 100)
-  [console.log(`Doplhins wins`)];
-else if (Koalas_score > Dolphins_score && Koalas_score >= 100) {
-  console.log(`Koalas wins`);
+avgDolp = Dolphins_score / 3;
+avgkoal = Koalas_score / 3;
+
+if (avgDolp > avgkoal && avgDolp >= 100) {
+  console.log("Dolphins wins");
+} else if (avgkoal > avgDolp && avgkoal >= 100) {
+  console.log("Koalas wins");
 } else {
-  console.log(`No one wins`);
+  console.log("No team wins!");
 }
-//challenge4
-alert(`challenge #4`);
-
-const bill_input = Number(prompt(`How much on the bill value?`));
-const tip =
-  bill_input >= 50 && bill_input <= 300 ? bill_input * 0.15 : bill_input * 0.2;
+//challenge #4
+value = Number(prompt("How much bill ? "));
+tips = value > 50 && value <= 300 ? value * 0.2 : value * 0.15;
+total = value + tips;
 console.log(
-  `The bill was ${bill_input},the tip was ${tip},and the total value ${
-    bill_input + tip
-  }`
+  `The bill was ${value}, the tips was ${tips}, and the total value ${total}.`
 );
