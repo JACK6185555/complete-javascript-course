4bd537b1672f2b9dec11f0088b1c531493f3a195
JACK61855555 <wongkalok0618@gmail.com>
2022-11-08 10:24:13 +0800

    Update collection.js

--- 09-Data-Structures-Operators/starter/collection.js
+++ 09-Data-Structures-Operators/starter/collection.js
@@ -17,12 +17,11 @@ const game = {
       'Kimmich',
       'Goretzka',
       'Coman',
-      'Muller',
       'Gnarby',
       'Lewandowski',
     ],
     [
-      'Bruki',
+      'Burki',
       'Schulz',
       'Hummels',
       'Akanji',
@@ -35,44 +34,11 @@ const game = {
       'Gotze',
     ],
   ],
-  score: '4.0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th,2037',
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnardy', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th , 2037',
   odds,
 };
-
 //1
 const [players1, players2] = game.players;
 console.log(players1, players2);
-
-//2
-const [goalkeeper, ...other_players] = players1;
-console.log(`Team1(Goalkeeper):${goalkeeper} \nOther_Players:${other_players}`);
-
-const [goalkeeper_2, ...other_players_2] = players2;
-console.log(
-  `Team2(Goalkeeper): ${goalkeeper_2} \nOther_Players:${other_players_2}`
-);
-
-//3
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-//4
-var players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
-
-//5
-const { team1, draw, team2 } = game.odds;
-console.log(team1, draw, team2);
-
-//6
-const printGoals = function (...players) {
-  console.log(
-    `Scored players:[${players}]\nNumber of scored player:${players.length}`
-  );
-};
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-
-//7
-team1 < team2 && console.log(`Team 1 is more likely to win`);
-team1 > team2 && console.log(`Team 2 is more likely to win`);
