8c7cfb9dba72592681ad9ab866d10147c073583e
JACK61855555 <wongkalok0618@gmail.com>
2022-11-20 17:29:32 +0800

    Update collection.js

--- 09-Data-Structures-Operators/starter/collection.js
+++ 09-Data-Structures-Operators/starter/collection.js
@@ -39,23 +39,22 @@ const game = {
   date: 'Nov 9th , 2037',
   odds,
 };
-//challenge #1
+//challenge 1
 //1
 const [players1, players2] = game.players;
 console.log(players1, players2);
 
 //2
-const [gk1, ...fieldPlayers] = players1;
-console.log(
-  `The goalkeeper is ${gk1}.\nThe remaining 10 field players are ${fieldPlayers}`
-);
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
 
 //3
 const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
 //4
-const player1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
 
 //5
 const { team1: team1, x: draw, team2: team2 } = game.odds;
@@ -63,41 +62,31 @@ console.log(team1, draw, team2);
 
 //6
 const printGoals = (...players) => {
-  console.log(`${players.length} were scored in total`);
-  console.log(`${players}`);
+  console.log(`${players.length} players were scored in total`);
 };
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 printGoals(...game.scored);
 
 //7
-team1 < team2 && console.log('Team1 is more likely to win');
+console.log(`Team 1 is more likely to win`) || team1 < team2;
+team2 < team1 && console.log(`Team 2 is more likely to win`);
 
-team2 < team1 && console.log('Team2 is more likely to win');
-
-//challenge #2
-
+//challenge 2
 //1
-const scored = [...game.scored];
-for (const [i, key] of game.scored.entries()) {
-  console.log(`Goal ${i + 1}:${key}`);
+for (const [i, players] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}:${players}`);
 }
 
 //2
-const odd = Object.values(game.odds);
-console.log(odd);
-let average = 0;
-for (const a of odd) average += a;
-average /= odd.length;
-console.log(average);
+let sum = 0;
+for (const a of Object.values(game.odds)) sum += a;
+sum /= 3;
+console.log(sum);
 
 //3
-for (const [team, odd] of Object.entries(game.odds)) {
-  const name = team === 'x' ? 'draw' : `victory ${game[team]}`;
-  console.log(`Odd of ${name}:${odd}`);
+for (const [team, score] of Object.entries(game.odds)) {
+  const Name = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${Name}:${score}`);
 }
-
-//challenge #3
-
 const gameEvents = new Map([
   [17, '⚽Goal'],
   [36, '♾️Substitution'],
@@ -111,21 +100,3 @@ const gameEvents = new Map([
   [80, '⚽Goal'],
   [92, '🟡Yellow card'],
 ]);
-//1
-const events = gameEvents.values();
-console.log(new Set(events));
-
-//2
-gameEvents.delete(64);
-
-//3
-const time = [...gameEvents.keys()].pop();
-console.log(time);
-
-console.log(`An event happened on average:${time / gameEvents.size}`);
-
-//4
-for (const [name, event] of gameEvents.entries()) {
-  const half = name <= 45 ? 'FIRST' : 'SECOND';
-  console.log(`[${half} HALF]${name}:${event}`);
-}
