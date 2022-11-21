cf4b2c33bfb904c594f3fabe36f3e7b1cc7e5e3a
JACK61855555 <wongkalok0618@gmail.com>
2022-11-21 16:27:01 +0800

    Update collection.js

--- 09-Data-Structures-Operators/starter/collection.js
+++ 09-Data-Structures-Operators/starter/collection.js
@@ -87,6 +87,7 @@ for (const [team, score] of Object.entries(game.odds)) {
   const Name = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`Odd of ${Name}:${score}`);
 }
+
 const gameEvents = new Map([
   [17, '⚽Goal'],
   [36, '♾️Substitution'],
@@ -100,3 +101,44 @@ const gameEvents = new Map([
   [80, '⚽Goal'],
   [92, '🟡Yellow card'],
 ]);
+//1
+const events = gameEvents.values();
+console.log(new Set(events));
+
+//2
+gameEvents.delete(64);
+
+//3
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened ,on average,every ${time / gameEvents.size} minutes`
+);
+
+//4
+for (const [i, s] of gameEvents.entries()) {
+  const half = i < 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half}HALF]${i}:${s}`);
+}
+
+//challenge 4
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  console.log(document.querySelector('textarea').value);
+
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
